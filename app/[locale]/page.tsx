ded7cea9cbd8e90d6463228531fca3c2f4109e6d
mario.ferreyra <mario.ferreyra@knotion.com>
2026-01-26 16:38:21 -0600

    fix: without cookies

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -8,6 +8,11 @@ import Hero from "@/modules/home/presentation/components/Hero";
 export default function Home() {
   return (
     <main className="bg-[#FAF3E0]">
+      {/* ----------- SECTION 1 ------------ */}
+      <section>
+        <Hero />
+      </section>
+
       {/* ----------- SECTION 3 ------------ */}
       <section className="w-full flex items-center justify-center">
         <Contact />
