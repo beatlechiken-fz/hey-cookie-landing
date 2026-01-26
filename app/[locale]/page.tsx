ff79f0723b15961940d5322ef6bc329418ff2562
mario.ferreyra <mario.ferreyra@knotion.com>
2026-01-26 09:55:03 -0600

    fix: pan-y

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,28 +1,13 @@
 "use client";
 
 import FooterBar from "@/core/components/footer-bar/FooterBar";
-import { useBreakpoint } from "@/core/hooks/useBreakpoint";
-import { CustomBreakpoint } from "@/core/types/general";
 import Contact from "@/modules/home/presentation/components/Contact";
 import Cookies from "@/modules/home/presentation/components/Cookies";
 import Hero from "@/modules/home/presentation/components/Hero";
 
 export default function Home() {
-  const breakpointsConfig: Record<
-    CustomBreakpoint,
-    { min?: number; max?: number }
-  > = {
-    cxxs: { max: 1 },
-    cxs: { min: 2, max: 839 },
-    csm: { min: 840, max: 1022 },
-    cmd: { min: 1023, max: 1199 },
-    clg: { min: 1200 },
-  };
-
-  const breakpoint = useBreakpoint(breakpointsConfig);
-
   return (
-    <main className="bg-[#FAF3E0]">
+    <main className="bg-[#FAF3E0] touch-pan-y">
       {/* ----------- SECTION 1 ------------ */}
       <section>
         <Hero />
